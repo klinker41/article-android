811df975a2d7c49d3078c41ece6cdc3946dd5960
Jake Klinker <jklinker1@gmail.com>
2016-10-30 11:40:04 -0700

    add sample layout

--- sample/src/main/java/xyz/klinker/article/SampleActivity.java
+++ sample/src/main/java/xyz/klinker/article/SampleActivity.java
@@ -16,8 +16,15 @@
 
 package xyz.klinker.article;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
