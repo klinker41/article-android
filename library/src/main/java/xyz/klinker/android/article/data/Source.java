26537ac1cc3e0a24a5bc4c1d95e634ce16154a39
Jake Klinker <jklinker1@gmail.com>
2017-04-04 12:42:18 -0700

    update copyright information

--- library/src/main/java/xyz/klinker/android/article/data/Source.java
+++ library/src/main/java/xyz/klinker/android/article/data/Source.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Jacob Klinker
+ * Copyright (C) 2017 Jake Klinker
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
